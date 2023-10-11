15e93311e1f814bdc785b97b545fb6ad29279903
hiimo <115462482+hiimo@users.noreply.github.com>
2023-10-11 18:03:44 +0300

    Update iseisevul3.java

---
+++ iseisevul3.java
@@ -0,0 +1,9 @@
+public class iseisevul3 {
+    public static void main(String[] args) {
+		System.out.print("\"jutum�rgid\"\n"); 
+		System.out.print("Kaldu\\//"+"\n");		
+		System.out.print("Kas ��kirjutad� �\\� samamoodi?");	
+
+	}
+
+}
