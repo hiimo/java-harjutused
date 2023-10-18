f122b306ad17b113e2f6162d2d1a220c964b8630
hiimo <115462482+hiimo@users.noreply.github.com>
2023-10-18 17:34:52 +0300

    Update ules20.java

--- ules20.java
+++ ules20.java
@@ -9,11 +9,12 @@ public class ules20 {
         
         int kymnesArv = 0;
         int pikkus = kahendsysteemiArv.length();
-        
+        //kui i on 0 ja kui see on vaiksem kui pikkus siis lisab 1 nii kaua kuni see ei ole
         for (int i = 0; i < pikkus; i++) {
             char bit = kahendsysteemiArv.charAt(pikkus - 1 - i);
             if (bit == '1') {
                 kymnesArv += Math.pow(2, i);
+                // siin astendatakse 2-ga kuni i-ni valja
             }
         }
         
