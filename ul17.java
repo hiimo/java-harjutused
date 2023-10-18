3303b371f86fe27a8a25b2dcd537caa1ff6f73a1
hiimo <115462482+hiimo@users.noreply.github.com>
2023-10-18 17:15:44 +0300

    Update ul17.java

--- ul17.java
+++ ul17.java
@@ -1,11 +1,13 @@
 public class ul17 {
     public static void main(String[] args) {
+        //arvud mis pidid olema
         int[] arvud = {3, 5, 4, 12, 5};
         
         System.out.print("\nPaarisarvud: ");
         for (int arv : arvud) {
             if (arv % 2 == 0) {
                 System.out.print(arv + " ");
+                //jagas integeris olevad arvud kahega
             }
         }
     }
