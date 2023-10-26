497d64a713b8f66186dcff2774908c94a788382a
hiimo <115462482+hiimo@users.noreply.github.com>
2023-10-26 07:39:39 +0300

    Update ul17.java

--- ul17.java
+++ ul17.java
@@ -2,12 +2,13 @@ public class ul17 {
     public static void main(String[] args) {
         //arvud mis pidid olema
         int[] arvud = {3, 5, 4, 12, 5};
-        
+    try {
         System.out.print("\nPaarisarvud: ");
         for (int arv : arvud) {
             if (arv % 2 == 0) {
                 System.out.print(arv);
                 //jagas massiivis olevad arvud kahega
+        } catch (Exception e) {
             }
         }
     }
