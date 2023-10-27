964f1a4d58e787dfbead90ca29da75b63e80c105
hiimo <115462482+hiimo@users.noreply.github.com>
2023-10-27 10:56:00 +0300

    Update ul262.java

---
+++ ul26/ul262.java
@@ -0,0 +1,9 @@
+public class ul262 {
+    public static void main(String[] args) {
+        ul26 vaip = vaip.vaiploomine();
+        vaip.vaibahind();
+        double hind = vaip.vaibahindarvutamine();
+        System.out.println("Vaiba hind on: " + hind + " eurot");
+    }
+}
+
